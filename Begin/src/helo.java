bdde15aaa670912c284303358ac29d1fc364058d
rhakq <rhakq@DESKTOP-UNO6NEQ>
2024-03-12 17:58:22 +0900

    eclipse_java

---
+++ Begin/src/helo.java
@@ -0,0 +1,11 @@
+
+public class helo { //클래스의 이름 == helo
+
+	public static void main(String[] args) { //진입점
+		// TODO Auto-generated method stub //블록
+		System.out.println("허서진"); //console창에 출력
+  //문장 사이의 구분 ;
+
+	}
+
+}
