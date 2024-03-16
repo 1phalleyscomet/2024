bb2ea27a1a5ecd5327562040c237b9fdb33193a4
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-03-16 13:38:55 +0900

    Add files via upload

---
+++ for.java
@@ -0,0 +1,12 @@
+for (int i=0; i<6;i++)
+  System.out.println("Hello");
+
+for (int i=1;i<6;i++)
+System.out.println(i); //i++==i+1
+
+//for (초기값;종료조건;증분){statements;}
+
+/* while문
+i=0;
+while(종료조건){statements;
+           i=i+1}*/
