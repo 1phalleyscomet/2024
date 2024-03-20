1b25461db75e4ef4ce5ddcf8f1ecb11a8d1ffd14
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-03-20 16:38:10 +0900

    Update 소스4-3.c

--- 소스4-3.c
+++ 소스4-3.c
@@ -2,9 +2,9 @@
 void main()
 {
 	int a = 10, b;
-	b = a++; //b=a -> a+1 ¼öÇà console a=11 b=10
+	b = a++; //b=a -> a+1 순서 console a=11 b=10
 	printf("%d", b);
 
-	b = ++a; //a+1 -> b=a ¼öÇà console a=11 b=11 
+	b = ++a; //a+1 -> b=a 순서 console a=11 b=11 
 	printf("%d", b);
 }
