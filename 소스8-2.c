bcd551800142846872386cd6fb978e6b1ab37757
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-04-02 12:06:19 +0900

    Add files via upload

---
+++ 소스8-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+	int i, k;
+	for (i = 1; i <= 9; i++)
+	{
+		for (k = 2; k <= 9; k++)
+		{
+			printf("%2dX%2d=%2d", i, k, k * i);
+			
+		}
+		printf("\n");
+	}
+}
