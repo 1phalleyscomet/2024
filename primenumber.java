aa379044ea210c6efee8fa12ff8141f0a1948338
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-04-08 19:58:02 +0900

    Create primenumber.java

---
+++ primenumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class main {
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	sc.close();
+	
+	boolean isPrime = true;
+	for (int n %i ==0) {
+		isPrime = false;
+		break;
+		}
+		
+		if (isPrime == true)
+			System.out.println("Prime number");
+		else System.out.println("not Prime");
+		}
+		}
