b46c79e9fe8db5cdd5bd6950e70fe0cf8c61f4d0
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-04-15 22:23:51 +0900

    Create log.java

---
+++ log.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class main{
+  public static void main(String[] args){
+    Scanner key = new Scanner(System.in):
+      int n =key.nextInt();
+    int b = key.nextInt();
+    key.close();
+
+  double t1=Math.log(n);
+    double t2=Math.log(b);
+    ind d=(int)(t1/t2)+1;
+    System.out.println(d):
+      }
+}
