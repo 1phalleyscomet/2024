d159a0bb4644be2a462d6d32e503fe96a0c2f0fd
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-04-15 22:09:38 +0900

    Create MathLibrary.java

---
+++ MathLibrary.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class main{
+  public static void main(String[] args){
+    System.out.println(Math.abs(-5)+" "+Math.abs(-5.3)); //abs절댓값
+    System.out.println(Math.sqrt(2)); //루트
+      System.out.println(Math.sin(Math.PI/4.0)); //라디안 단위
+      System.out.println(Math.cos(Math.PI/4.0));
+      System.out.println(Math.tan(Math.PI/4.0));
+      System.out.println(Math.ceil(6.1)); //올림
+      System.out.println(Math.round(6.49); //반올림
+      System.out.println(Math.floor(6.1)); //내림
+      System.out.println(Math.log(2.0));
+  }
+}
