5bd376ceab55dd18015552a4d08bd9dbb08d2578
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-04-18 11:35:35 +0900

    Add files via upload

---
+++ test13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+	int dice[6] = { 0 };
+	int i;
+	int sum = 0;
+
+	srand(time(NULL));
+
+	printf("주사위를 20번 던진 결과:\n");
+
+	for (i = 0; i < 20; i++) {
+		int roll = rand() % 6; 
+		printf("%d ", roll+1);
+		sum += roll+1;
+	}
+	
+
+	printf("\n주사위를 20번 던진 결과의 합: %d", sum);
+
+
+
+	return 0;
+}
