2e79d74273d0af3610bb6e426ed0ba5f021b6242
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-04-29 21:52:49 +0900

    Create 두지점의거리.java

---
+++ 두지점의거리.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class main{
+  public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+    int T =sc.nextInt();
+    for (int t = 0; t<T; t++){
+      int n = sc.nextInt();'
+      int[] a = nwe int[n];
+  for (int i =0 ; i<a.length; i++) 
+      a[i]==sc.nextInt();
+      Arrays.sort(a);
+      System.out.print(a[n-i]-a[0]*2);
+  }
+ sc.close();
+  
+  }
+}
