33b22b32b70111c620f1e3a6f120d29bb38a7564
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-05-08 16:38:24 +0900

    Create string.java

---
+++ string.java
@@ -0,0 +1,26 @@
+public class main{
+  public static void main(String[] args){
+    char tc=65; //특정 문자에 저장된 숫자코드
+    int ti=65;
+    System.out.println(ti+" "+tc);
+  }
+}
+
+public class main{
+  public static void main(String[] args){
+    char tc='b';
+    int ti=(int) tc;
+    System.out.println(ti+" "+tc);
+  }
+}
+
+
+public class main{
+  public static void main(String[] args){
+    String t='Jeju'; //문자 4개+null 1개-> 5개 동시 선언
+    char cc = t.charAt(0);
+    int ci=(int) cc;
+    System.out.println(cc+" "+ci);
+  }
+}
+//'a'-> 선언 1개 "a" -> 선언 2
