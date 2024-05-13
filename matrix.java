90374b5de252e3a8cdf232b39d15c66c52641774
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-05-13 19:35:45 +0900

    Update matrix.java

--- matrix.java
+++ matrix.java
@@ -1,12 +1,12 @@
 public class main{
-  static void ShowMat(int[][]m){
+static void ShowMat(int[][]m){
     for(int i=0;i<m.length;i++){
+    	for(int j=0;j<m[i].length;j++)
       System.out.print(m[i][j]+" ");
       System.out.println();
     }
-    System.out.println();
-  }
-  public static void main(String[] args){
+      System.out.println();
+  }static void main(String[] args){
     int[][]a={{1,2,3},{4,5,6},{7,8,9}};
     int [][]b=new int[3][2];
     b[0][0]=11; b[0][1]=12;
