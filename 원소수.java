260b3ae1a12fc64df96e551f58b9849c163447d8
1phalleyscomet <162965685+1phalleyscomet@users.noreply.github.com>
2024-05-13 19:35:22 +0900

    Update 원소수.java

--- 원소수.java
+++ 원소수.java
@@ -1,11 +1,20 @@
 public class main{
-
+static void ShowMat(int[][]m){
+    for(int i=0;i<m.length;i++){
+    	for(int j=0;j<m[i].length;j++)
+      System.out.print(m[i][j]+" ");
+      System.out.println();
+    }
+      System.out.println();
+  }
 public static void main(String[] args){
   int[][] a={{1,2,3},{4,5,6},{7,8,9}};
   int [][]b={{10,20},{30,40,50}};
   ShowMat(a);
   ShowMat(b);
   a[1]=b[0];
+  ShowMat(a);
+  b[0][0]=-100;
   ShowMat(a);
 }
 }
